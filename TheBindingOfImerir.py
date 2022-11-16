6eec933d56680bcaa62d1790f992bbde46ba08d4
NinoZore <ncls.nino13@gmail.com>
2022-11-16 13:50:53 +0100

    ça fonctionne

--- TheBindingOfImerir.py
+++ TheBindingOfImerir.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import random
 import time
 import sys
@@ -8,7 +9,6 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-
 def rot_center(image, angle):
     
     loc = image.get_rect().center  #rot_image is not defined 
@@ -42,26 +42,30 @@ def KeyboardInput():
             player.rect.y += PLAYER_SPEED
         elif (player.rect.y < screen_height and player.rect.y > (screen_height - PLAYER_SPEED)):   
             player.rect.y = screen_height + player.image.get_height()
-
+    
     if keys[pygame.K_LEFT]: 
-        tears = Tears(SHOT_LEFT) 
-        #tears.rect.x -= PLAYER_SHOOT_VELOCITY
-        tears_list.add(tears)
+            tears = Tears(SHOT_LEFT) 
+            #tears.rect.x -= PLAYER_SHOOT_VELOCITY
+            tears_list.add(tears)
+            pygame.time.set_timer(COULDOWN,PLAYER_SHOOT_SPEED)
 
     elif keys[pygame.K_RIGHT]:
-        tears = Tears(SHOT_RIGHT)
-        #tears.rect.x += PLAYER_SHOOT_VELOCITY
-        tears_list.add(tears)
+            tears = Tears(SHOT_RIGHT)
+            #tears.rect.x += PLAYER_SHOOT_VELOCITY
+            tears_list.add(tears)
+            pygame.time.set_timer(COULDOWN,PLAYER_SHOOT_SPEED)
 
     elif keys[pygame.K_UP]:
         tears = Tears(SHOT_UP)
         #tears.rect.y -= PLAYER_SHOOT_VELOCITY
         tears_list.add(tears)
+        pygame.time.set_timer(COULDOWN,PLAYER_SHOOT_SPEED)
 
     elif keys[pygame.K_DOWN]:
         tears = Tears(SHOT_DOWN)
         #tears.rect.y += PLAYER_SHOOT_VELOCITY
         tears_list.add(tears)
+        pygame.time.set_timer(COULDOWN,PLAYER_SHOOT_SPEED)
 
 class Block(pygame.sprite.Sprite):
 
@@ -150,19 +154,66 @@ def options():
         screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
 
-        FULLSCREEN_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(screen_width/2, screen_height/2 - 150), 
-            text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(screen_width/2, screen_height/2),
-            text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(screen_width/2, screen_height/2 + 150), 
-            text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BACK = Button(image=None, pos=(screen_width/2, 460), 
+        AFFICHAGE_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(screen_width/2, screen_height/2 - 150), 
+            text_input="Affichage", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        SONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(screen_width/2, screen_height/2),
+            text_input="Sons", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        CONTROLS_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(screen_width/2, screen_height/2 + 150), 
+            text_input="Controls", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BACK = Button(image=None, pos=(screen_width/2, screen_height/2 + 300), 
             text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(screen)
 
-        for button in [FULLSCREEN_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+        for button in [AFFICHAGE_BUTTON, SONS_BUTTON, CONTROLS_BUTTON]:
+            button.changeColor(OPTIONS_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if AFFICHAGE_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    menu_affichage()
+                if SONS_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    menu_sons()
+                if CONTROLS_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    menu_controls()
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    return 0
+                    #main_menu()
+
+        pygame.display.update()
+
+def menu_affichage():
+    global screen_height
+    global screen_width
+    global FULLSCREEN
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        screen.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the AFFICHAGE_SETTINGS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(screen_width/2, 260))
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+
+        FULLSCREEN_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(screen_width/2, screen_height/2 - 150), 
+            text_input="FULLSCREEN", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        SD_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(screen_width/2, screen_height/2),
+            text_input="720p", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        HD_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(screen_width/2, screen_height/2 + 150), 
+            text_input="1080p", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BACK = Button(image=None, pos=(screen_width/2, screen_height/2 + 300), 
+            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS) 
+        OPTIONS_BACK.update(screen)
+
+        for button in [FULLSCREEN_BUTTON, SD_BUTTON, HD_BUTTON]:
             button.changeColor(OPTIONS_MOUSE_POS)
             button.update(screen)
 
@@ -172,20 +223,115 @@ def options():
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if FULLSCREEN_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    pygame.display.toggle_fullscreen()
+                if SD_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
+                    screen_width = 1080
+                    screen_height = 720
+                    pygame.display.set_mode((screen_width, screen_height))
+
+                if HD_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
                     screen_width = 1920
                     screen_height = 1080
                     pygame.display.set_mode((screen_width, screen_height))
 
-                    pygame.display.toggle_fullscreen()
-                if OPTIONS_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
-                    print("test")
-                if QUIT_BUTTON.checkForInput(OPTIONS_MOUSE_POS):
-                    print("test")
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     return 0
                     #main_menu()
 
         pygame.display.update()
+
+def menu_sons():
+    global screen_height
+    global screen_width
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        screen.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the SOUNDS_SETTINGS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(screen_width/2, 260))
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+
+        MODIFIER4 = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(screen_width/2, screen_height/2 - 150), 
+            text_input="MODIFIER4", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        MODIFIER5 = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(screen_width/2, screen_height/2),
+            text_input="MODIFIER5", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        MODIFIER6 = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(screen_width/2, screen_height/2 + 150), 
+            text_input="MODIFIER6", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BACK = Button(image=None, pos=(screen_width/2, screen_height/2 + 300), 
+            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(screen)
+
+        for button in [MODIFIER4, MODIFIER5, MODIFIER6]:
+            button.changeColor(OPTIONS_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if MODIFIER4.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if MODIFIER5.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if MODIFIER6.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    return 0
+                    #main_menu()
+
+        pygame.display.update()
+
+def menu_controls():
+    global screen_height
+    global screen_width
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        screen.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("This is the CONTROLS_SETTINGS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(screen_width/2, 260))
+        screen.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+
+        MODIFIER1 = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(screen_width/2, screen_height/2 - 150), 
+            text_input="MODIFIER1", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        MODIFIER2 = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(screen_width/2, screen_height/2),
+            text_input="MODIFIER2", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        MODIFIER3 = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(screen_width/2, screen_height/2 + 150), 
+            text_input="MODIFIER3", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BACK = Button(image=None, pos=(screen_width/2, screen_height/2 + 300), 
+            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(screen)
+
+        for button in [MODIFIER1, MODIFIER2, MODIFIER3]:
+            button.changeColor(OPTIONS_MOUSE_POS)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if MODIFIER1.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if MODIFIER2.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if MODIFIER3.checkForInput(OPTIONS_MOUSE_POS):
+                    print("en developement")
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    return 0
+                    #main_menu()
+
+        pygame.display.update()
+
 
 def main_menu():
     while True:
@@ -224,6 +370,9 @@ def main_menu():
 
         pygame.display.update()
 
+
+
+
 isaac_pp="Sprite/isaac.png"
 nino_pp="Sprite/nino.png"
 
@@ -247,15 +396,18 @@ SHOT_DOWN = 270
 
 pygame.init()
 pygame.mixer.init()
+pygame.display.init()
 
 
 # Set the height and width of the screen
 screen_width = 1080
 screen_height = 720
 
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height), HWSURFACE | DOUBLEBUF | RESIZABLE)
 pygame.display.set_caption("Menu")
-BG = pygame.image.load("assets/Background.jpg")
+
+BG = pygame.image.load("assets/Background.jpg").convert_alpha()
+pygame.transform.scale(BG, (screen_width, screen_height))
 
 if(FULLSCREEN == 1):
     pygame.display.toggle_fullscreen()
@@ -266,8 +418,13 @@ if(FULLSCREEN == 1):
 
 main_menu()
 
-BG = pygame.image.load("assets/Background.png")
+BG = pygame.image.load("assets/Background.png").convert_alpha()
+BG = pygame.transform.scale(BG, (screen_width, screen_height))
+rect = BG.get_rect()
+rect = rect.move((screen_width / 2, screen_height / 2))
+screen.blit(BG, rect)
 
+screen = pygame.display.set_mode((screen_width, screen_height))
 #debut jeux 
 block_list = pygame.sprite.Group()
 tears_list = pygame.sprite.Group()
@@ -297,7 +454,10 @@ done = False
 clock = pygame.time.Clock()
  
 score = 0
- 
+
+COULDOWN = pygame.USEREVENT + 0
+
+
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():
@@ -305,10 +465,11 @@ while not done:
             done = True
  
     # Clear the screen
-    screen.fill(WHITE)
+    screen.blit(BG, (0, 0))
     KeyboardInput()
     #if(tears_list.has(Tears) == True):
-        #blocks_hit_list = pygame.sprite.spritecollide(tears, block_list, True)
+    pygame.sprite.groupcollide(tears_list ,block_list ,True ,True)
+    #blocks_hit_list = pygame.sprite.spritecollide(tears, block_list, True)
         
         #for block in blocks_hit_list:
         #    score +=1
@@ -316,7 +477,6 @@ while not done:
     tears_list.update()
             #pygame.mixer.music.load("miam.mp3")
             #pygame.mixer.music.play()
-    
     all_sprites_list.draw(screen)
     # Limit to 60 frames per second
     clock.tick(60)
